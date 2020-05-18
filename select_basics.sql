36712be95dbd3cae84cb69083e89659b3e4053f6
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-05-17 18:59:58 -0700

    Update select_basics.sql

--- select_basics.sql
+++ select_basics.sql
@@ -11,7 +11,6 @@ FROM world
 WHERE name IN ('Sweden', 'Norway','Denmark');
 
 -- 3. Show the country and area for countries with an area between 200,000 and 250,000.
-
 SELECT name, area 
 FROM world
 WHERE area BETWEEN 200000 AND 250000;
