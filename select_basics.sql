d7602f964e6638fe7bc12eb8a045df64f6cc9387
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-05-17 19:03:23 -0700

    Create select_basics.sql

---
+++ select_basics.sql
@@ -0,0 +1,16 @@
+-- SELECT basics
+
+-- 1. Show the population of Germany.
+SELECT population 
+FROM world
+WHERE name = 'Germany';
+
+-- 2. Show the name and population for Sweden, Norway and Denmark.
+SELECT name, population 
+FROM world
+WHERE name IN ('Sweden', 'Norway','Denmark');
+
+-- 3. Show the country and area for countries with an area between 200,000 and 250,000.
+SELECT name, area 
+FROM world
+WHERE area BETWEEN 200000 AND 250000;
